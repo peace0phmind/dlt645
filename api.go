ae2a4496a89dffff7b3b360711b3b09f47dd413f
mind <peace0phmind@gmail.com>
2024-11-07 17:43:25 +0800

    remove useless code

--- api.go
+++ api.go
@@ -2,16 +2,6 @@ package dlt645
 
 import "github.com/shopspring/decimal"
 
-type Config struct {
-	Protocol P
-}
-
-func NewConfig() *Config {
-	cfg := &Config{}
-	cfg.Protocol = PV2007
-	return cfg
-}
-
 type Value struct {
 	Name  string
 	Unit  string
@@ -21,9 +11,4 @@ type Value struct {
 type Client interface {
 	Read(addr string, dic DIC) (*Value, error)
 	BatchRead(addr string, dics []DIC) ([]*Value, error)
-}
-
-type Packager interface {
-	Encode(f *Frame) ([]byte, error)
-	Decode(df []byte) (*Frame, error)
 }
